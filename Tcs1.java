eebd926c2fe45072e80dc4eac1fa04a9431483db
Adil-Mirza <adilmirza600@gmail.com>
2021-01-27 14:18:21 +0530

    first

---
+++ Tcs1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Tcs1 
+{
+	public static void main(String[] args) 
+	{ Scanner obj=new Scanner(System.in);
+	  //int id=obj.nextInt();
+	  int a=20;
+	  int d=150;
+
+	  float per=((float)(a)/(float)(d))*100;
+		System.out.printf("%.2f",per);
+		String name="ADil";
+	}
+}
